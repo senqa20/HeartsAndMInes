f9cd0161e25ab7b70cdaf15a03c50734a0c66d1c
senqa20 <34680260+senqa20@users.noreply.github.com>
2023-07-06 16:58:37 +0200

    5
    5

--- H&M_V69.brf_sumava/core/def/param.hpp
+++ H&M_V69.brf_sumava/core/def/param.hpp
@@ -399,7 +399,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_GAMEPLAY_CHEM"]);
         values[] = {0,1};
         texts[] = {$STR_DISABLED,$STR_ENABLED};
-        default = 1;
+        default = 0;
     };
     class btc_p_spect { // Spectrum devices
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_GAMEPLAY_SPECT"]);
